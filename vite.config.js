383d6723e06ea26168882f19e253d3bd560a7f21
Legends0024 <manavjoshi0024@gmail.com>
2025-07-02 15:04:17 +0530

    SOME CHANGES IN vute.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // For Netlify/Vercel, use _redirects file in public/
-});
+  publicDir: 'public', // This is the default, but you can specify it explicitly
+})
